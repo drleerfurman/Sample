dc1b1a84f13379a41b2a023a443c8bf09d28470c
drleerfurman <128236417+drleerfurman@users.noreply.github.com>
2025-09-26 19:37:57 +0300

    r
    42

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+string findcolor(string name)
+{
+    if (name == "red") {return "\033[31m";}
+    else if (name == "blue") {return "\033[34m";}
+    else {return "none";}
+}
+
+string findcolor2(string name)
+{
+    if (name == "red") {return "\033[41m";}
+    else if (name == "blue") {return "\033[44m";}
+    else {return "none";}
+}
 
 int main()
 {
@@ -39,7 +55,66 @@ int main()
     cout << "Pick console font color (background): ";
     cin >> back;
     
+    string compiledfore = findcolor(fore);
+    string compiledback = findcolor2(back);
+    
+    cout << compiledfore;
+    cout << compiledback;
+    
+    cout << "hjafihaeof \n heiahgfouigsui \n vhanuibeig \n";
+    cout << "\033[0m";
+    
     // 3
+    
+    int x = 16; int y = 8;
+    char symbol = '*'; string compiledci = findcolor("red"); string compiledco = "\033[0m";
+    string buffer = "";
+    // cin >> ...
+    if (compiledci == "none") {compiledci = "";}
+    buffer += compiledci;
+    for (int cy = 0; cy < y; cy++)
+    {
+        if ((cy == 0) || (cy == (y - 1)))
+        {
+            for (int cx = 0; cx < x; cx++)
+            {
+                buffer += symbol;
+            }
+        }
+        else
+        {
+            for (int cx = 0; cx < x; cx++)
+            {
+                if ((cx == 0) || (cx == (x - 1)))
+                {
+                    buffer += symbol;
+                }
+                else
+                {
+                    buffer += ' ';
+                }
+            }
+        }
+        buffer += '\n';
+    }
+    buffer += compiledco;
+    cout << buffer;
+    
+    // 4
+    srand(time(0));
+    int array[64]; int latest_max; int latest_min;
+    for (int i = 0; i < 64; i++)
+    {
+        array[i] = rand() - 1000000000;
+    }
+    // Simplfiable into one loop
+    latest_max = array[0]; latest_min = array[0];
+    for (int i = 1; i < 64; i++)
+    {
+        if (array[i] > latest_max) {latest_max = array[i];}
+        if (array[i] < latest_min) {latest_min = array[i];}
+    }
+    cout << "Max: " << latest_max << "\nMin: " << latest_min << "\n";
     
     return 0;
 }
